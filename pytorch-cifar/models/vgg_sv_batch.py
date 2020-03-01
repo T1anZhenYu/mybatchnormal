e9500a87ad634e23a7bcc47a1453b50f7c351edf
tian <t18215595124@gmail.com>
2020-03-01 17:12:52 +0800

    sv bn

--- pytorch-cifar/models/vgg_sv_batch.py
+++ pytorch-cifar/models/vgg_sv_batch.py
@@ -23,10 +23,10 @@ class SV_BatchNorm2d(nn.BatchNorm2d):
 
         # calculate running estimates
         if self.training:
-            mean = input.mean(dim=(2,3),keepdim=True)
+            mean = input.mean(dim=(0,2,3),keepdim=True)
             # print('mean size:', mean.size())
             # use biased var in train
-            var = (input - mean).pow(2).mean(dim=(2,3),keepdim=True)
+            var = (input - mean).pow(2).mean(dim=(0,2,3),keepdim=True)
             mean = mean.squeeze()
             var = var.squeeze()
             # print('variance size:', var.size())
@@ -34,17 +34,17 @@ class SV_BatchNorm2d(nn.BatchNorm2d):
 
             # print("n:",n)
             with torch.no_grad():
-                self.running_mean = exponential_average_factor * mean.mean(dim=0)\
+                self.running_mean = exponential_average_factor * mean\
                     + (1 - exponential_average_factor) * self.running_mean
                 # update running_var with unbiased var
-                self.running_var = exponential_average_factor * var.mean(dim=0)*n/(n-1)\
+                self.running_var = exponential_average_factor * var*n/(n-1)\
                     +(1 - exponential_average_factor) * self.running_var
                 # for i in range(var.size(0)):
                 #     self.running_var = exponential_average_factor * var[i] * n / (n - 1)\
                 #         + (1 - exponential_average_factor) * self.running_var
                     # self.running_var = exponential_average_factor * var * n / (n - 1)\
                         # + (1 - exponential_average_factor) * self.running_var
-            input = (input - mean[:, :, None, None]) / (torch.sqrt(var[:, :, None, None] + self.eps))
+            input = (input - mean[None, :, None, None]) / (torch.sqrt(var[None, :, None, None] + self.eps))
         else:
             mean = self.running_mean
             var = self.running_var

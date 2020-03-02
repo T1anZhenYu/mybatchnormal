44ac6ef4a3811d53f71311de9804bfd66f8b4d97
tian <t18215595124@gmail.com>
2020-03-02 13:18:05 +0800

    gpu

--- pytorch-cifar/main.py
+++ pytorch-cifar/main.py
@@ -32,11 +32,7 @@ args = parser.parse_args()
 
 os.makedirs(args.dir, exist_ok=True)
 
-os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
-use_cuda = torch.cuda.is_available()
-print('current gpu:')
-print(torch.cuda.current_device())
-print("Let's use", torch.cuda.device_count(), "GPUs!\n")
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
@@ -54,10 +50,10 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR100(root='./cifar_data/', train=True, download=True, transform=transform_train)
+trainset = torchvision.datasets.CIFAR10(root='./cifar_data/', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR100(root='./cifar_data/', train=False, download=True, transform=transform_test)
+testset = torchvision.datasets.CIFAR10(root='./cifar_data/', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -92,8 +88,7 @@ else:
 # net = SENet18()
 # net = ShuffleNetV2(1)
 # net = EfficientNetB0()
-net = torch.nn.DataParallel(net).cuda()
-cudnn.benchmark = True
+net = net.to(device)
 # if device == 'cuda':
 #     net = torch.nn.DataParallel(net)
 #     cudnn.benchmark = True
@@ -134,7 +129,7 @@ def train(epoch):
     correct = 0
     total = 0
     for batch_idx, (inputs, targets) in enumerate(trainloader):
-        inputs, targets = inputs.cuda(), targets.cuda(async=True)
+        inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
         loss = criterion(outputs, targets)
@@ -158,7 +153,7 @@ def test(epoch):
     total = 0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.cuda(), targets.cuda()
+            inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
             loss = criterion(outputs, targets)
 
